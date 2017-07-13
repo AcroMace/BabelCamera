9d55e864dd6056cc8ee4f4498bfcdab2a8b852f9
Andy Cho <acromace@gmail.com>
2017-07-13 12:42:35 -0400

    Initial Commit

---
+++ BabelCamera/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BabelCamera
+//
+//  Created by Andy Cho on 2017-07-13.
+//  Copyright © 2017 AcroMace. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
